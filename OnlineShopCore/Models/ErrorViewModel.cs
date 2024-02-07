ec8ecff79861875d8f13c48ce825038862097c4c
mary <marya.6t@gmail.com@gmail.com>
2024-02-07 15:55:45 -0800

    Add project files.

---
+++ OnlineShopCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineShopCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
