b0ec009175d5b6bab6c6ddf645713080248b286e
mobocomputer <mobocomputer@TEST-SYSTEM>
2024-02-10 08:43:59 -0800

    Added navigations properties to product and item.

--- OnlineShopCore/Models/Item.cs
+++ OnlineShopCore/Models/Item.cs
@@ -3,8 +3,14 @@
     public class Item
     {
         public int Id { get; set; }
-        public Product Product { get; set; }
         public Decimal Price { get; set; }
         public int QuantityInStock { get; set; }
+        public int ProductId { get; set; }
+
+        #region navigations
+
+        public Product Product { get; set; }
+
+        #endregion
     }
 }
--- OnlineShopCore/Models/Product.cs
+++ OnlineShopCore/Models/Product.cs
@@ -6,10 +6,12 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int ItemId { get; set; }
 
         #region navigations
 
         public List<CategoryProduct> CategoryProducts { get; set; }
+        public Item Item { get; set; }
 
         #endregion
     }
